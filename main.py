22e4c7a45fed7a032e9df8feaf5f90fe3f4eeb71
Guilherme Ribeiro <guilherme.ribeiro062002@gmail.com>
2024-12-16 11:03:03 -0300

    16/12/24

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from mongo import perform_mongo_action
 from postgres import perform_postgres_action
+# from mongo import perform_mongo_action
 
 def main():
-    print("Testing MongoDB...")
-    perform_mongo_action()
-
     print("Testing PostgreSQL...")
     perform_postgres_action()
+
+    print("Testing MongoDB...")
+    # perform_mongo_action()
+
+    
 
 if __name__ == "__main__":
     main()
--- postgres.py
+++ postgres.py
@@ -1,34 +1,108 @@
 import psycopg2
 import time
+import csv
+from psycopg2._psycopg import connection
 
+def create_table(conn:connection):
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS customers (
+            id SERIAL PRIMARY KEY,
+            first_name character varying NULL,
+            last_name character varying NULL,
+            company character varying NULL,
+            city character varying NULL,
+            country character varying NULL,
+            phone character varying NULL,
+            email character varying NULL
+        )
+    '''
+    )
+    conn.commit()
+    cursor.close()
+
+def delete_table(conn: connection):
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        DELETE FROM customers
+        '''
+    )
+    cursor.close()
+
+def insert(conn:connection):
+    cursor = conn.cursor()
+    
+    with open('customers-500000.csv') as csv_file:
+        data = csv.reader(csv_file, delimiter=',')
+        next(data)
+        with open('./logs/tempos_postgres.txt', 'a+') as log_file:
+            for linha in data:
+                start_time = time.perf_counter() #inicio do tempo
+                # ['19705', 'fdca2d73C3Bf883', 'Spencer', 'Fitzpatrick', 'Mcintyre-Paul', 'Francishaven', 'Andorra', '(496)135-0763x0675', '426.072.4187x9174', 'ebruce@jarvis-nash.info', '2021-10-08', 'https://www.gutierrez-barber.info/']
+                cursor.execute(
+                    '''
+                    INSERT INTO customers (first_name, last_name, company, city, country, phone, email)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s)
+                    ''',
+                    (linha[2], linha[3], linha[4], linha[5], linha[6], linha[7], linha[9])
+                )
+                conn.commit()
+                end_time = time.perf_counter()            
+                #fim do tempo em milissegundos
+                elapsed = (end_time - start_time) * 1000
+                log_file.write(f"{elapsed:.4f}\n")
+    cursor.close()
+        
+def select_all(conn: connection):
+    cursor = conn.cursor()
+    start_time = time.perf_counter() # inicio do tempo
+    cursor.execute(
+        '''
+        SELECT * FROM customers
+        '''
+    )
+    rows = cursor.fetchall()
+    end_time = time.perf_counter() # fim do tempo
+    elapsed = (end_time - start_time) * 1000 # tempo em milissegundos
+    cursor.close()
+    print(f"Select all action took {elapsed:.4f} milliseconds")
+
+def calculate_average():
+    with open('./logs/tempos_postgres.txt', 'r') as log_file:
+        lines = log_file.readlines()
+        total = 0
+        for line in lines:
+            total += float(line)
+        print(total)
+        print(len(lines))
+        return total / len(lines)
+    
 def perform_postgres_action():
-    start = time.time()
 
     conn = psycopg2.connect(
-        dbname="test_db",
-        user="postgres",
-        password="postgres",
+        dbname="tcc",
+        user="tcc",
+        password="tcc",
         host="localhost",
         port="5432"
     )
-    cursor = conn.cursor()
 
-    # Teste de criação de tabela e inserção
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS test_table (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(50),
-            value INTEGER
-        )
-    """)
-    cursor.execute("INSERT INTO test_table (name, value) VALUES (%s, %s)", ("Test", 123))
-    conn.commit()
+    # delete_table(conn)
+    # create_table(conn)
+    # insert(conn)
 
-    cursor.execute("SELECT * FROM test_table WHERE name = %s", ("Test",))
-    print("PostgreSQL Fetched: ", cursor.fetchone())
+    # time = calculate_average()
+    select_all(conn)
+    # print(f"PostgreSQL action took {time:.2f} miliseconds")
+    # cursor.execute("SELECT * FROM test_table WHERE name = %s", ("Test",))
+    # print("PostgreSQL Fetched: ", cursor.fetchone())
+    # cursor.close()
+    # conn.close()
 
-    cursor.close()
-    conn.close()
+    # elapsed = time.time() - start
+    # print(f"PostgreSQL action took {elapsed:.2f} seconds")
 
-    elapsed = time.time() - start
-    print(f"PostgreSQL action took {elapsed:.2f} seconds")
+
+
