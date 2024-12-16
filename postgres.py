13d219f6f88de1334cd4a5a709acf408fbf44092
guilherme-ribeiro27 <guilherme.ribeiro062002@gmail.com>
2024-12-16 12:39:53 -0300

    new dataset

--- postgres.py
+++ postgres.py
@@ -7,15 +7,18 @@ def create_table(conn:connection):
     cursor = conn.cursor()
     cursor.execute(
         '''
-        CREATE TABLE IF NOT EXISTS customers (
-            id SERIAL PRIMARY KEY,
-            first_name character varying NULL,
-            last_name character varying NULL,
-            company character varying NULL,
-            city character varying NULL,
-            country character varying NULL,
-            phone character varying NULL,
-            email character varying NULL
+        CREATE TABLE IF NOT EXISTS products (
+            asin character varying PRIMARY KEY,
+            title character varying NULL,
+            img_url character varying NULL,
+            productURL character varying NULL,
+            stars numeric NULL,
+            reviews integer NULL,
+            price money NULL,
+            listPrice money NULL,
+            categoryName character varying NULL,
+            isBestSeller boolean NULL,
+            boughtInLastMonth integer NULL
         )
     '''
     )
@@ -26,7 +29,7 @@ def delete_table(conn: connection):
     cursor = conn.cursor()
     cursor.execute(
         '''
-        DELETE FROM customers
+        DELETE FROM products
         '''
     )
     cursor.close()
@@ -34,33 +37,36 @@ def delete_table(conn: connection):
 def insert(conn:connection):
     cursor = conn.cursor()
     
-    with open('customers-500000.csv') as csv_file:
+    with open('dataset_amazon') as csv_file:
         data = csv.reader(csv_file, delimiter=',')
         next(data)
-        with open('./logs/tempos_postgres.txt', 'a+') as log_file:
-            for linha in data:
-                start_time = time.perf_counter() #inicio do tempo
-                # ['19705', 'fdca2d73C3Bf883', 'Spencer', 'Fitzpatrick', 'Mcintyre-Paul', 'Francishaven', 'Andorra', '(496)135-0763x0675', '426.072.4187x9174', 'ebruce@jarvis-nash.info', '2021-10-08', 'https://www.gutierrez-barber.info/']
-                cursor.execute(
-                    '''
-                    INSERT INTO customers (first_name, last_name, company, city, country, phone, email)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s)
-                    ''',
-                    (linha[2], linha[3], linha[4], linha[5], linha[6], linha[7], linha[9])
-                )
-                conn.commit()
-                end_time = time.perf_counter()            
-                #fim do tempo em milissegundos
-                elapsed = (end_time - start_time) * 1000
-                log_file.write(f"{elapsed:.4f}\n")
+        # with open('./logs/tempos_postgres.txt', 'a+') as log_file:
+        totalTime = 0
+        for linha in data:
+            start_time = time.perf_counter() #inicio do tempo
+            # ['19705', 'fdca2d73C3Bf883', 'Spencer', 'Fitzpatrick', 'Mcintyre-Paul', 'Francishaven', 'Andorra', '(496)135-0763x0675', '426.072.4187x9174', 'ebruce@jarvis-nash.info', '2021-10-08', 'https://www.gutierrez-barber.info/']
+            cursor.execute(
+                '''
+                INSERT INTO products (asin, title, img_url, productURL, stars, reviews, price, listPrice, categoryName, isBestSeller, boughtInLastMonth)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                ''',
+                (linha[0], linha[1], linha[2], linha[3], linha[4], linha[5], linha[6], linha[7], linha[8], linha[9], linha[10])
+            )
+            conn.commit()
+            end_time = time.perf_counter()            
+            #fim do tempo em milissegundos
+            elapsed = (end_time - start_time) * 1000
+            totalTime += elapsed
+            # log_file.write(f"{elapsed:.4f}\n")
     cursor.close()
+    print(f"Insert action took {totalTime:.4f} milliseconds")
         
 def select_all(conn: connection):
     cursor = conn.cursor()
     start_time = time.perf_counter() # inicio do tempo
     cursor.execute(
         '''
-        SELECT * FROM customers
+        SELECT * FROM products
         '''
     )
     rows = cursor.fetchall()
@@ -69,16 +75,6 @@ def select_all(conn: connection):
     cursor.close()
     print(f"Select all action took {elapsed:.4f} milliseconds")
 
-def calculate_average():
-    with open('./logs/tempos_postgres.txt', 'r') as log_file:
-        lines = log_file.readlines()
-        total = 0
-        for line in lines:
-            total += float(line)
-        print(total)
-        print(len(lines))
-        return total / len(lines)
-    
 def perform_postgres_action():
 
     conn = psycopg2.connect(
@@ -89,12 +85,11 @@ def perform_postgres_action():
         port="5432"
     )
 
-    # delete_table(conn)
-    # create_table(conn)
-    # insert(conn)
+    delete_table(conn)
+    create_table(conn)
+    insert(conn)
 
-    # time = calculate_average()
-    select_all(conn)
+    # select_all(conn)
     # print(f"PostgreSQL action took {time:.2f} miliseconds")
     # cursor.execute("SELECT * FROM test_table WHERE name = %s", ("Test",))
     # print("PostgreSQL Fetched: ", cursor.fetchone())
